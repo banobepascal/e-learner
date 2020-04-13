6f070c4e7a96fdda1383b7586ae61cb028554d1f
banobepascal <banobepascal@gmail.com>
2020-04-13 21:45:24 +0300

    added course-details component to courses-page

--- src/Pages/Courses-Page/Courses-Page.component.jsx
+++ src/Pages/Courses-Page/Courses-Page.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import CourseDetails from "../../components/Course-Details/Course-Details.component";
 import CoursesOverview from "../../components/Courses-Overview/Courses-Overview.component";
 
 const CoursesPage = ({ match }) => {
