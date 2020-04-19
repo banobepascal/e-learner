4e2c30b9e98d3be73fa578555ff0bc42e3a314ee
banobepascal <banobepascal@gmail.com>
2020-04-19 22:17:18 +0300

    added styles to signin-signup component

--- src/Pages/SignIn-SignUp-Page/SignIn-SignUp.component.jsx
+++ src/Pages/SignIn-SignUp-Page/SignIn-SignUp.component.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+
+import "./SignIn-SignUp.styles";
 
 const SignInSignUp = () => {
-    return (  
-        <div></div>
-    );
-}
- 
+  return <div></div>;
+};
+
 export default SignInSignUp;
