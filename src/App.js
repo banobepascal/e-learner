84cd152d6acdbea4f30a11718b6af191a7411092
banobepascal <banobepascal@gmail.com>
2020-04-28 23:36:09 +0300

    ft - storing user data in app

--- src/App.js
+++ src/App.js
@@ -19,8 +19,21 @@ class App extends Component {
   unsubscribefromAuth = null;
 
   componentDidMount() {
-    this.unsubscribefromAuth = auth.onAuthStateChanged((user) => {
-      createUserProfileDocument(user);
+    this.unsubscribefromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
