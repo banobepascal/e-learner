8e3c19d1a6b9b9183f77e64f878bdaad7941b898
banobepascal <banobepascal@gmail.com>
2020-04-29 18:15:35 +0300

    ch - created auth-buttons component to handle third party auth

---
+++ src/components/Auth-Buttons/Auth-Buttons.styles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AuthButtonsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1em;
+  i {
+    font-size: 1.2em;
+  }
+`;
+
