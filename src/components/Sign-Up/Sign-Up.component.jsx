fd25a1a284aece867d0aac4d840c72105650d786
banobepascal <banobepascal@gmail.com>
2020-05-03 00:04:14 +0300

    bg - fixed a responsive bug in sign-up component

--- src/components/Sign-Up/Sign-Up.component.jsx
+++ src/components/Sign-Up/Sign-Up.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
+import { Form} from "react-bootstrap";
 
 import FormInput from "../Form-Input/Form-Input.component";
 import CustomButton from "../Custom-Button/Custom-Button.component";
