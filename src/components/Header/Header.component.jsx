ac7ed614c9cc1adca10c813be40fefed7dfa54db
banobepascal <banobepascal@gmail.com>
2020-05-08 22:56:00 +0300

    ch - implemented selectors to handle currentUser of session

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import CustomButton from "../Custom-Button/Custom-Button.component";
 
 import { auth } from "../../firebase/firebase.utils";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+
 import {
   HeaderContainer,
   LogoContainer,
@@ -64,8 +67,8 @@ const Header = ({ currentUser }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 export default connect(mapStateToProps)(Header);
