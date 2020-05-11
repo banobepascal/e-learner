5508c7aee9c012f34d38a16fd71fe8d85dea3b84
banobepascal <banobepascal@gmail.com>
2020-05-11 23:50:06 +0300

    ch - changed course routes and links to display a single course

--- src/Pages/Courses-Page/Courses-Page.component.jsx
+++ src/Pages/Courses-Page/Courses-Page.component.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import CoursePreview from "../../components/Course-Preview/Course-Preview.component";
 import CoursesOverview from "../../components/Courses-Overview/Courses-Overview.component";
 
 const CoursesPage = ({ match }) => {
   return (
     <div>
       <Route exact path={`${match.path}`} component={CoursesOverview} />
-      <Route path={`${match.path}/course/:slug`} component={CoursePreview}/>
     </div>
   );
 };
