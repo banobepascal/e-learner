c5a02cb82a9f83da5a465610d9a1b937a41e7eda
banobepascal <banobepascal@gmail.com>
2020-05-13 20:46:02 +0300

    ch - adding axios dependency to perform fetch

--- src/components/Search/Search.component.jsx
+++ src/components/Search/Search.component.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import { FormContainer, InputField } from "./Search.styles";
 
 class Search extends Component {
+  state = {
+    query: "",
+    results: {},
+    loading: false,
+    message: "",
+  };
+
+  handleOnInputChange = (event) => {
+    const query = event.target.value;
+    this.setState({ query, loading: true, message: "" });
+  };
+
   render() {
     return (
       <FormContainer>
@@ -11,6 +24,7 @@ class Search extends Component {
             id="search"
             type="text"
             placeholder="Search for category or course"
+            onChange={this.handleOnInputChange}
           />
         </InputField>
         <button type="submit">
