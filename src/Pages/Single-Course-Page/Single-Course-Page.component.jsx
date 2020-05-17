e3e0c3353be763665c1c9dff39736bbb560603b0
banobepascal <banobepascal@gmail.com>
2020-05-17 21:24:15 +0300

    bg - fix single course rendering

--- src/Pages/Single-Course-Page/Single-Course-Page.component.jsx
+++ src/Pages/Single-Course-Page/Single-Course-Page.component.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import CoursePreviewContainer from "../../components/Course-Preview/Course-Preview-Container.component";
+import { fetchCoursesStartAsync } from "../../redux/courses/courses.actions";
 
-const SingleCoursePage = ({ match }) => {
-  return (
-    <div>
-      <Route path={`${match.path}/:slug`} component={CoursePreviewContainer} />
-    </div>
-  );
-};
+class SingleCoursePage extends React.Component {
+  componentDidMount() {
+    const { fetchCoursesStartAsync } = this.props;
+    fetchCoursesStartAsync();
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <Route
+          path={`${match.path}/:slug`}
+          component={CoursePreviewContainer}
+        />
+      </div>
+    );
+  }
+}
 
-export default SingleCoursePage;
+const mapDispatchToProps = (dispatch) => ({
+  fetchCoursesStartAsync: () => dispatch(fetchCoursesStartAsync()),
+});
+
+export default connect(null, mapDispatchToProps)(SingleCoursePage);
