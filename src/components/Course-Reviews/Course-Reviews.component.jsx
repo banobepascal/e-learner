16c786cab675681ea7dd0d2ca225a81fa710510f
banobepascal <banobepascal@gmail.com>
2020-05-21 17:42:36 +0300

    ch(modified course reviews section) :
     - added react-stars for ratings
     - added new text in comments
     - changed stylings for reviews
     - refactored some code
    
    Finishes

--- src/components/Course-Reviews/Course-Reviews.component.jsx
+++ src/components/Course-Reviews/Course-Reviews.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactStars from "react-stars";
 
 import {
   ReviewContainer,
@@ -6,30 +7,45 @@ import {
   ReviewTitleContainer,
   ReviewAuthorDetails,
 } from "./Course-Reviews.styles";
-const CourseReviews = ({ reviews, ratings }) => {
+const CourseReviews = ({ ratings, authorImageUrl, authorName }) => {
   return (
     <ReviewContainer>
       <h5>Reviews</h5>
       <ReviewsContent>
         <ReviewTitleContainer>
           <ReviewAuthorDetails>
-            <div>image</div>
+            <img src={authorImageUrl} alt="" />
             <div>
-              <div>author name</div>
-              <div>date</div>
+              <div>{authorName}</div>
+              <i>2 weeks ago</i>
             </div>
           </ReviewAuthorDetails>
           <div>
-            {" "}
-            <i className="fa fa-star" aria-hidden="true"></i>
-            {ratings}
+            <ReactStars count={ratings} size={24} color1={"#ffd700"} />
           </div>
         </ReviewTitleContainer>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-          adipisci, saepe explicabo obcaecati et aliquam molestias ipsa magni
-          beatae cum accusantium sint sunt repudiandae ratione doloremque
-          dolorem quia at architecto?
+          The lessons are prepared in a short and crisp fashion and in an easy
+          language, which makes it easier for me to understand the concept. They
+          are also well structured so that no part of any topic is left out.
+        </p>
+      </ReviewsContent>
+      <ReviewsContent>
+        <ReviewTitleContainer>
+          <ReviewAuthorDetails>
+            <img src={authorImageUrl} alt="" />
+            <div>
+              <div>{authorName}</div>
+              <i>1 weeks ago</i>
+            </div>
+          </ReviewAuthorDetails>
+          <div>
+            <ReactStars count={ratings} size={24} color1={"#ffd700"} />
+          </div>
+        </ReviewTitleContainer>
+        <p>
+          Its been an awesome experience so far. Its practically my first online
+          full option training
         </p>
       </ReviewsContent>
     </ReviewContainer>
