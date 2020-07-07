2b3e8f9f3bc44d90e27ef6155dbf0f833f4d73be
banobepascal <banobepascal@gmail.com>
2020-07-07 23:54:24 +0300

    bg - fix redux dispatch in app

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => 
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
